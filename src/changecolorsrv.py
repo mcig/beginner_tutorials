3a86347c7f3ef0b42e9850a4b79fefa635ea28cd
Mustafa Cig Gokpinar <cig-mustafa.2001@hotmail.com>
2022-11-21 09:52:43 +0300

    lecture 1

--- src/changecolorsrv.py
+++ src/changecolorsrv.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python3
 
 import rospy
-from std_srvs.srv import Empty, EmptyResponse
+from std_srvs.srv import Empty
 from turtlesim.srv import *
 
 rospy.init_node('setcolornode', anonymous=True)
-color_r = 120#rospy.get_param('r')
-color_g = 60#rospy.get_param('g')
-color_b = 50#rospy.get_param('b')
+print("1")
+
+color_r = rospy.get_param('r')
+color_g = rospy.get_param('g')
+color_b = rospy.get_param('b')
 
 print("r: ", color_r)
 print("g: ", color_g)
 print("b: ", color_b)
 
+print("imdat")
 rospy.wait_for_service('clear')
+print("2")
 
 rospy.set_param('/turtlesim/background_r', color_r)
 rospy.set_param('/turtlesim/background_g', color_g)
@@ -22,4 +26,6 @@ rospy.set_param('/turtlesim/background_b', color_b)
 clear_background = rospy.ServiceProxy('clear', Empty)
 clear_background()
 
+spawnTurtle = rospy.ServiceProxy('spawn', Spawn)
+spawnTurtle(2, 2, 0, 'turtle2')
 
